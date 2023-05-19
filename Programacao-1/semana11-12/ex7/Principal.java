96ad102c6c81a338a2486d237e3b07eba595a047
Samuca47Prog <samuelsimao47@gmail.com>
2023-05-19 19:54:45 -0300

    d

---
+++ Programacao-1/semana11-12/ex7/Principal.java
@@ -0,0 +1,9 @@
+public class Principal{
+    public static void main(String[] args){
+        double[] array = {0, 4325.65, 55555.4, 1.2};
+
+        Exercicio7 ex = new Exercicio7();
+
+        System.out.println("Posição maior valor: " + ex.posicaoMaiorValor(array));
+    }
+}
