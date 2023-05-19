03d4b6ba60ea1bc8643ab6b7a87db3dfd938506a
Samuca47Prog <samuelsimao47@gmail.com>
2023-05-19 00:49:17 -0300

    exercicio 1 completo

--- Programacao-1/semana11-12/ex1/Principal.java
+++ Programacao-1/semana11-12/ex1/Principal.java
@@ -16,8 +16,26 @@ public class Principal {
         /* Exercício C */
         int[] arrayC = new int[10];
 
-        for (int i=0; i < arrayB.length; i++)
-            arrayB[i] = i * i;
+        for (int i=0; i < (arrayC.length / 2); i++)
+            arrayC[i] = i+1;
+
+        for (int i=(arrayC.length / 2); i < arrayC.length; i++)
+            arrayC[i] = arrayC[i - (arrayC.length / 2)] * 10;
+
+        /* Exercício D */
+        int[] arrayD = new int[10];
+
+        /*  
+         * 3 4 7 12 19 28 39 52 67 84 
+         *-1 1 3 5  7  9  11 13 15 17
+        */
+
+        arrayD[0] = 3;
+
+        for (int i=1 ; i < arrayD.length; i++){
+            arrayD[i] = i*i +3;
+        }
+
 
         System.out.println();
     }
