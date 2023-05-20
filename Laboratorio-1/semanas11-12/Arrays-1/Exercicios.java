e2b750dc70cdc1b962d36d113fd2f918177a096d
Samuca47Prog <samuelsimao47@gmail.com>
2023-05-20 16:45:54 -0300

    até o exercício 17

--- Laboratorio-1/semanas11-12/Arrays-1/Exercicios.java
+++ Laboratorio-1/semanas11-12/Arrays-1/Exercicios.java
@@ -98,4 +98,242 @@ public class Exercicios {
 
     }
 
+
+
+    public int[] exercicio6(int[] array1, int[] array2){
+        int[] retorno = null;
+        int tamanhoRetorno = 0;
+
+
+        if(array1 != null)
+            tamanhoRetorno += array1.length;
+        if(array2 != null)
+            tamanhoRetorno += array2.length;
+
+        retorno = new int[tamanhoRetorno];
+
+
+        if(array1 != null){
+            for(int i=0; i<array1.length; i++){
+                retorno[i] = array1[i];
+            }
+        }
+        if(array2 != null){
+            for(int i=0; i<array2.length; i++){
+                retorno[i+array1.length] = array2[i];
+            }
+        }
+
+        return retorno;
+
+    }
+
+
+
+    public int[] exercicio7(int[] array1, int[] array2){
+        int[] retorno = null;
+        int tamanhoRetorno = 0;
+
+        int index = 0;
+
+
+        if(array1 != null)
+            tamanhoRetorno += array1.length;
+        if(array2 != null)
+            tamanhoRetorno += array2.length;
+
+        retorno = new int[tamanhoRetorno];
+
+        // Posiciona os números negativos
+        if(array1 != null){
+            for(int i=0; i<array1.length; i++){
+                if(array1[i] < 0){
+                    retorno[index] = array1[i];
+                    index++;
+                }
+                    
+            }
+        }
+        if(array2 != null){
+            for(int i=0; i<array2.length; i++){
+                if(array2[i] < 0){
+                    retorno[index] = array2[i];
+                    index++;
+                }
+            }
+        }
+
+        // Posiciona os números npositivos
+        if(array1 != null){
+            for(int i=0; i<array1.length; i++){
+                if(array1[i] >= 0){
+                    retorno[index] = array1[i];
+                    index++;
+                }
+                    
+            }
+        }
+        if(array2 != null){
+            for(int i=0; i<array2.length; i++){
+                if(array2[i] >= 0){
+                    retorno[index] = array2[i];
+                    index++;
+                }
+            }
+        }
+
+        return retorno;
+
+    }
+
+
+
+    public void exercicio8(char[] chars){
+        for(char c : chars){
+            System.out.print(c);
+        }
+    }
+
+
+
+    public double exercicio9(int[] array){
+        double soma = 0.0;
+        int numeroDeElementos = 0;
+
+        for(int i : array){
+            soma += i;
+            numeroDeElementos++;
+        }
+
+        if(numeroDeElementos != 0){
+            return soma / numeroDeElementos;
+        }
+        else{
+            return 0;
+        }
+
+    }
+
+
+
+    public double exercicio10(int[] array){
+        double somaPares = 0.0;
+        int numeroDeElementosPares = 0;
+
+        for(int i : array){
+            if(i%2 == 0){
+                somaPares += i;
+                numeroDeElementosPares++;
+            }
+        }
+
+        if(numeroDeElementosPares != 0){
+            return somaPares / numeroDeElementosPares;
+        }
+        else{
+            return 0;
+        }
+
+    }
+
+
+
+    public int exercicio11(int[] array, int posicao){
+        if(array != null){
+            return array[posicao];
+        }
+        else{
+            return -1;
+        }
+
+    }
+
+
+
+    public boolean exercicio12(double[] array){
+        boolean haNegativos = false;
+
+        for(double d : array){
+            if(d < 0){
+                haNegativos = true;
+            }
+        }
+
+        return haNegativos;
+    }
+
+
+
+    public int exercicio13(int[] array){
+        int maiorNumero = array[0];
+
+        for(int i=1; i<array.length; i++ ){
+            if(array[i] > maiorNumero){
+                maiorNumero = array[i];
+            }
+        }
+
+        return maiorNumero;
+    }
+
+
+    public int exercicio14(int[] array){
+        int posicaoMaiorNumero = 0;
+
+        for(int i=1; i<array.length; i++ ){
+            if(array[i] > array[posicaoMaiorNumero]){
+                posicaoMaiorNumero = i;
+            }
+        }
+
+        return posicaoMaiorNumero;
+    }
+
+
+    public int exercicio15(int[] array){
+        int menorNumero = array[0];
+
+        for(int i=1; i<array.length; i++ ){
+            if(array[i] < menorNumero){
+                menorNumero = array[i];
+            }
+        }
+
+        return menorNumero;
+    }
+
+
+    public int exercicio16(int[] array){
+        int posicaoMenorNumero = 0;
+
+        for(int i=1; i<array.length; i++ ){
+            if(array[i] < array[posicaoMenorNumero]){
+                posicaoMenorNumero = i;
+            }
+        }
+
+        return posicaoMenorNumero;
+    }
+
+
+    public boolean exercicio17(int[] array1, int[] array2){
+
+        if(array1 == null || array2 == null)
+            return false;
+
+        if(array1.length == array2.length){
+            for(int i=0; i<array1.length; i++){
+                if(array1[i] != array2[i]){
+                    return false;
+                }
+            }
+        }
+        else {
+            return false;
+        }
+
+        return true;
+
+    }
+
 }
--- Laboratorio-1/semanas11-12/Arrays-1/Principal.java
+++ Laboratorio-1/semanas11-12/Arrays-1/Principal.java
@@ -4,9 +4,12 @@ public class Principal{
     public static void main(String[] args){
         Exercicios exercicios = new Exercicios();
 
-        int[] arrayInt = {1, 2, 3, 4, 5, 6};
+        int[] arrayInt = {1, 2, 3, -4, 5, 6};
+        int[] arrayIn2 = {10, 20, 35, 47, -5, -61};
         String[] arrayString = {"Eu", "Memo", "Samuel"};
         boolean[] arrayBool = {true, false, false ,true, true};
+        char[] arrayChars = {'C', 'H', 'A', 'R'};
+        double[] arrayDouble = {1.2, 43.56, 8, -54.6, -10};
 
 
 
@@ -35,6 +38,66 @@ public class Principal{
 
 
 
-        System.out.println("Final");
+        System.out.println("\n\n Exercício 6");
+        System.out.println(Arrays.toString(exercicios.exercicio6(arrayInt, arrayIn2)));
+
+
+
+        System.out.println("\n\n Exercício 7");
+        System.out.println(Arrays.toString(exercicios.exercicio7(arrayInt, arrayIn2)));
+
+
+
+        System.out.println("\n\n Exercício 8");
+        exercicios.exercicio8(arrayChars);
+
+
+
+        System.out.println("\n\n Exercício 9");
+        System.out.println("Média: " + exercicios.exercicio9(arrayInt));
+
+
+
+        System.out.println("\n\n Exercício 10");
+        System.out.println("Média: " + exercicios.exercicio10(arrayInt));
+
+
+
+        System.out.println("\n\n Exercício 11");
+        System.out.println(exercicios.exercicio11(arrayInt, 2));
+
+
+
+        System.out.println("\n\n Exercício 12");
+        System.out.println("Há negativos? " + exercicios.exercicio12(arrayDouble));
+
+
+
+        System.out.println("\n\n Exercício 13");
+        System.out.println("Maior número: " + exercicios.exercicio13(arrayInt));
+
+
+
+        System.out.println("\n\n Exercício 14");
+        System.out.println("Posição do maior número: " + exercicios.exercicio14(arrayInt));
+
+
+
+        System.out.println("\n\n Exercício 15");
+        System.out.println("Menor número: " + exercicios.exercicio15(arrayInt));
+
+
+
+        System.out.println("\n\n Exercício 16");
+        System.out.println("Posição do menor número: " + exercicios.exercicio16(arrayInt));
+
+
+
+        System.out.println("\n\n Exercício 17");
+        System.out.println("Posição do menor número: " + exercicios.exercicio17(arrayInt, arrayIn2));
+
+
+
+        System.out.println("\n\nFinal");
     }
 }
